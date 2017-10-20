93983ed8a64f4cabcc67e3d665d3e9d4ee9a206b
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2017-10-20 16:44:37 -0200

    README
    Estrutura simples em html, css e javascript puro

---
+++ js/dados.js
@@ -0,0 +1,68 @@
+var controlefecha = 0;
+	
+	function esconderElemento1(){
+	    		
+		document.getElementById("elemento_1").style.display = "block";
+		document.getElementById("elemento_2").style.display = "none";
+		document.getElementById("elemento_3").style.display = "none";
+		document.getElementById("elemento_4").style.display = "none";
+		document.getElementById("elemento_5").style.display = "none";
+		document.getElementById("elemento_6").style.display = "none";
+		
+	}
+	
+	function esconderElemento2(){
+	
+		document.getElementById("elemento_2").style.display = "block";
+		document.getElementById("elemento_1").style.display = "none";
+		document.getElementById("elemento_3").style.display = "none";
+		document.getElementById("elemento_4").style.display = "none";
+		document.getElementById("elemento_5").style.display = "none";
+		document.getElementById("elemento_6").style.display = "none";
+		
+	}
+	
+	function esconderElemento3(){
+	
+		document.getElementById("elemento_3").style.display = "block";
+		document.getElementById("elemento_1").style.display = "none";
+		document.getElementById("elemento_2").style.display = "none";
+		document.getElementById("elemento_4").style.display = "none";
+		document.getElementById("elemento_5").style.display = "none";
+		document.getElementById("elemento_6").style.display = "none";
+		
+	}
+	
+	function esconderElemento4(){
+	
+		document.getElementById("elemento_4").style.display = "block";
+		document.getElementById("elemento_1").style.display = "none";
+		document.getElementById("elemento_2").style.display = "none";
+		document.getElementById("elemento_3").style.display = "none";
+		document.getElementById("elemento_5").style.display = "none";
+		document.getElementById("elemento_6").style.display = "none";
+		
+	}
+	
+	function esconderElemento5(){	
+    
+		document.getElementById("elemento_5").style.display = "block";
+		document.getElementById("elemento_1").style.display = "none";
+		document.getElementById("elemento_2").style.display = "none";
+		document.getElementById("elemento_3").style.display = "none";
+		document.getElementById("elemento_4").style.display = "none";
+		document.getElementById("elemento_6").style.display = "none";
+		
+	}
+	
+	function esconderElemento6(){	    
+	
+		document.getElementById("elemento_6").style.display = "block";
+		document.getElementById("elemento_1").style.display = "none";
+		document.getElementById("elemento_2").style.display = "none";
+		document.getElementById("elemento_4").style.display = "none";
+		document.getElementById("elemento_5").style.display = "none";
+		document.getElementById("elemento_3").style.display = "none";
+		
+	}
+	
